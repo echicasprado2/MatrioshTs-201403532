4c503e853a68a56c32dafa31928727f7daf77788
EverChicas <everchicasp@gmail.com>
2020-09-23 02:19:28 -0600

    fix in treegraph

--- src/ASTGraph/NodeGraphAST.js
+++ src/ASTGraph/NodeGraphAST.js
@@ -19,12 +19,29 @@ class NodeGraphAST{
     }
 
     totalString(root){
+        root.tag = root.tag.replace("\"","");
+        root.tag = root.tag.replace("\"","");
+        root.tag = root.tag.replace("\'","");
+        root.tag = root.tag.replace("\'","");
+        root.tag = root.tag.replace("\`","");
+        root.tag = root.tag.replace("\`","");
+
         var codeGraph = "";
         var nodeN = `node${root.numberNode}(("${root.tag}"))\n`;
         if(root.tag != 'null'){
             for(var i = 0; i != root.children.length;i++){
                 if(root.children[i].tag != 'null'){
-                    var tagH = `node${root.children[i].numberNode}(("${root.children[i].tag}"))\n`;
+
+                    root.children[i].tag = root.children[i].tag.replace("\"","");
+                    root.children[i].tag = root.children[i].tag.replace("\"","");
+                    root.children[i].tag = root.children[i].tag.replace("\'","");
+                    root.children[i].tag = root.children[i].tag.replace("\'","");
+                    root.children[i].tag = root.children[i].tag.replace("\`","");
+                    root.children[i].tag = root.children[i].tag.replace("\`","");
+                    
+                    console.log(root.children[i].tag);
+
+                    var tagH = `node${root.children[i].numberNode}(("${root.children[i].tag} "))\n`;
                     var code = `node${root.numberNode} --> node${root.children[i].numberNode}\n`;
                     codeGraph += nodeN + tagH + code;
                     codeGraph += this.totalString(root.children[i]);
