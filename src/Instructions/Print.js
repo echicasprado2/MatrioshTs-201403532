cfd007937203d13bf6631303aca19dedf07bbdc9
EverChicas <everchicasp@gmail.com>
2020-09-24 11:17:22 -0600

    sotution for fix git pages

---
+++ src/Instructions/Print.js
@@ -0,0 +1,62 @@
+class Print extends Instruction {
+    constructor(linea, column, expresion) {
+        super(linea, column);
+        this.values = expresion;
+        this.translatedCode = "";
+    }
+    
+    /**
+     * obtener el codigo para la traduccion
+     */
+    getTranslated(){
+        this.translatedCode = "console.log(";
+        for(var i = 0;i < this.values.length;i++){
+            this.translatedCode += (i == 0)? this.values[i].getTranslated() : `, ${this.values[i].getTranslated()}`;
+        }
+        this.translatedCode += ");\n";
+        return this.translatedCode;
+    }
+
+    /**
+     * 
+     * @param {Environment actual} e  
+     */
+    translatedSymbolsTable(e){}
+
+    /**
+     * 
+     * @param {Enviroment} e 
+     */
+    executeSymbolsTable(e){
+        return "implementar este codigo"
+    }
+
+    /**
+     * 
+     * @param {*} e 
+     */
+    execute(e) {
+        var resultCadena = "";
+        var result;
+
+        for(var i = 0; i < this.values.length; i++){
+            result = this.values[i].getValue(e);
+            if(result != null){
+                
+                if(result.type.enumType == EnumType.STRING){
+                    if(result.value != "@vacio"){
+                        resultCadena += result.value;
+                    }
+                }else{
+                    resultCadena += result.value;
+                }
+            }else{
+                console.log("llega null al print");
+            }
+        }
+
+        PrintConsole.printLine(resultCadena);
+        return null; 
+    }
+    
+}
