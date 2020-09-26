f8f34fcabd730dfebabe5722c17df2b18ad9d095
EverChicas <everchicasp@gmail.com>
2020-09-26 10:56:45 -0600

    fix declaration with array value

--- src/Instructions/Declaration.js
+++ src/Instructions/Declaration.js
@@ -37,20 +37,38 @@ class Declaration extends Instruction {
     }
 
     if (this.value != null) {
+
+      this.translatedCode += " = "
+
       if (this.value instanceof Array) {
-        for (var i = 0; i < this.value.length; i++) {
-          if (i == 0) {
-            this.translatedCode += " = " + this.value[i].getTranslated();
-          } else {
-            this.translatedCode += "." + this.value[i].getTranslated();
-          }
-        }
+        this.translatedCode += this.getValueArray(this.value);
+      
       } else {
-        this.translatedCode += " = " + this.value.getTranslated();
+        this.translatedCode +=   this.value.getTranslated();
       }
+    
     }
     return this.translatedCode + ";\n";
   }
+
+  getValueArray(value){
+    var cadena = "[";
+
+    for(var i = 0; i < value.length; i++){
+        if(value[i] instanceof Array && i == 0){
+            cadena += `${this.getValueArray(value[i])}`;
+        }else if(value[i] instanceof Array){
+            cadena += `,${this.getValueArray(value[i])}`;
+        }else if(i == 0){
+            cadena += value[i].value;
+        }else{
+            cadena += `,${value[i].value}`;
+        }            
+    }
+    
+    cadena += "]";
+    return cadena;
+}
 
   translatedSymbolsTable(e) {
     for (var i = 0; i < this.ids.length; i++) {
@@ -77,26 +95,32 @@ class Declaration extends Instruction {
     if (this.typeDeclaration.enumType == EnumDeclarationType.CONST) {
       if (this.value == null) {
         for (var i = 0; i < this.ids.length; i++) {
-            ErrorList.addError(new ErrorNode(this.line,this.column,new ErrorType(EnumErrorType.SEMANTIC),`La constante: "${this.ids[i]}" no tiene asignacion de un valor, debe tener valor`,e.enviromentType));
-          }
-          return null;
+          ErrorList.addError(new ErrorNode(this.line,this.column,new ErrorType(EnumErrorType.SEMANTIC),`La constante: "${this.ids[i]}" no tiene asignacion de un valor, debe tener valor`,e.enviromentType));
         }
+        return null;
       }
+    }
       
-      if(this.value != null){
-        result = this.value.getValue(e);
+    if(this.value != null){
+
+      if(this.value instanceof Array){
+        ErrorList.addError(new ErrorNode(this.line,this.column,new ErrorType(EnumErrorType.SEMANTIC),`no se puede asignar un array a una variable`,e.enviromentType));
+        return null;
+      }
+
+      result = this.value.getValue(e);
+      
+      if(result.type.enumType == EnumType.ERROR){
+        ErrorList.addError(new ErrorNode(this.line,this.column,new ErrorType(EnumErrorType.SEMANTIC),`el valor da error`,e.enviromentType));
+        return null;
+      }
         
-        if(result.type.enumType == EnumType.ERROR){
-          ErrorList.addError(new ErrorNode(this.line,this.column,new ErrorType(EnumErrorType.SEMANTIC),`el valor da error`,e.enviromentType));
-          return null;
+      if(result.type.enumType == EnumType.ARRAY){
+        for(var i = 0; i < this.ids.length; i++){
+          ErrorList.addError(new ErrorNode(this.line,this.column,new ErrorType(EnumErrorType.SEMANTIC),`la variable ${this.ids[i]} no soporta un array`,e.enviromentType));
         }
-        
-        if(result.type.enumType == EnumType.ARRAY){
-          for(var i = 0; i < this.ids.length; i++){
-            ErrorList.addError(new ErrorNode(this.line,this.column,new ErrorType(EnumErrorType.SEMANTIC),`la variable ${this.ids[i]} no soporta un array`,e.enviromentType));
-          }
-          return null;  
-        }
+        return null;  
+      }
 
     }else{
       result = null;
@@ -107,14 +131,14 @@ class Declaration extends Instruction {
       if(result == null){
         e.insertNewSymbol(this.ids[i],new Symbol(this.line,this.column,this.ids[i],this.type,this.typeDeclaration,new Value(new Type(EnumType.NULL,null),"",0)));
       }else{
-          if(this.type.enumType == EnumType.NULL){
-            this.type = result.type;
-            e.insertNewSymbol(this.ids[i],new Symbol(this.line,this.column,this.ids[i],this.type,this.typeDeclaration,result,0));
-          }else if(this.type.enumType != result.type.enumType){
-            ErrorList.addError(new ErrorNode(this.line,this.column,new ErrorType(EnumErrorType.SEMANTIC),`El tipo de la variable no es el mismo que su valor : ${this.type.toString()} != ${result.type.toString()}`,e.enviromentType));
-          }else{
-            e.insertNewSymbol(this.ids[i],new Symbol(this.line,this.column,this.ids[i],this.type,this.typeDeclaration,result,0));
-          }
+        if(this.type.enumType == EnumType.NULL){
+          this.type = result.type;
+          e.insertNewSymbol(this.ids[i],new Symbol(this.line,this.column,this.ids[i],this.type,this.typeDeclaration,result,0));
+        }else if(this.type.enumType != result.type.enumType){
+          ErrorList.addError(new ErrorNode(this.line,this.column,new ErrorType(EnumErrorType.SEMANTIC),`El tipo de la variable no es el mismo que su valor : ${this.type.toString()} != ${result.type.toString()}`,e.enviromentType));
+        }else{
+          e.insertNewSymbol(this.ids[i],new Symbol(this.line,this.column,this.ids[i],this.type,this.typeDeclaration,result,0));
+        }
       }
 
     }
